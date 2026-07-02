4a26e68bd9705a10630c0835e28b3d69dfbfb9ba
Faithhhhh1 <faithlives01@gmail.com>
2026-07-02 11:56:23 +0530

    Create constants.js

---
+++ js/constants.js
@@ -0,0 +1,17 @@
+export const STEM_COLORS = Object.freeze({
+    BASE: "#4F7152",
+    DARK: "#355238",
+    LIGHT: "#8DBA92",
+    SHADOW: "#29402C"
+});
+
+export const STEM_WIDTHS = Object.freeze({
+    MEDIUM: 5,
+    SMALL: 3,
+    HERO: 9
+});
+
+export const STEM_ORIGIN = Object.freeze({
+    x: 540,
+    y: 1760
+});
