048aba63c11a86769ea88cdc764969ccb3e1d0fc
Faithhhhh1 <faithlives01@gmail.com>
2026-07-02 11:06:03 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
-import "./animation.js";
+import { createScene } from "./svg.js";
+import { createStemLayer } from "./stems.js";
+import { attachLilyToStem } from "./lily.js";
+import { attachTulipToStem } from "./tulip.js";
+import { attachSunflowerToStem } from "./sunflower.js";
+import { attachBlueFlowerToStem } from "./blueFlower.js";
+import { attachLeavesToStem } from "./leaves.js";
+import { createIntroMessage } from "./messages.js";
+
+const scene = document.getElementById("scene");
+const bouquetLayer = document.getElementById("bouquet-layer");
+
+const stems = createStemLayer();
+bouquetLayer.appendChild(stems);
+
+function getPoint() {
+    return {
+        x: 540 + (Math.random() - 0.5) * 300,
+        y: 900 + Math.random() * 300
+    };
+}
+
+const factories = [
+    attachLilyToStem,
+    attachTulipToStem,
+    attachSunflowerToStem,
+    attachBlueFlowerToStem
+];
+
+const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+group.setAttribute("id", "flowers");
+
+for (let i = 0; i < 18; i++) {
+
+    const { x, y } = getPoint();
+
+    const flower = factories[i % factories.length](x, y);
+    group.appendChild(flower);
+
+    const leaves = attachLeavesToStem([{ x, y }]);
+    group.appendChild(leaves);
+}
+
+stems.appendChild(group);
+stems.appendChild(createIntroMessage());
